cc4cae835a7ef661ae07fd80dfca7f28675feb4b
burnett <>
2001-05-28 23:47:37 +0000

    Rename algoritm, implement varous trigger bits using AO bit definitions.

---
+++ src/Trigger_dll.cxx
@@ -0,0 +1,26 @@
+//====================================================================
+//
+//====================================================================
+
+// DllMain entry point
+#include "GaudiKernel/DllMain.icpp"
+#include <iostream>
+void GaudiDll::initialize(void*) 
+{
+}
+
+void GaudiDll::finalize(void*) 
+{
+}
+extern void trigger_load();
+#include "GaudiKernel/FactoryTable.h"
+
+extern "C" FactoryTable::EntryList* getFactoryEntries() {
+  static bool first = true;
+  if ( first ) {  // Don't call for UNIX
+    trigger_load();
+    first = false;
+  }
+  return FactoryTable::instance()->getEntries();
+} 
+
