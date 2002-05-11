5617148e4f69ef8f7cf3480beaf06138ff80f251
burnett <>
2002-05-11 22:28:00 +0000

    follow conventions

--- src/Trigger_dll.cxx
+++ src/Trigger_dll.cxx
@@ -12,23 +12,15 @@ void GaudiDll::initialize(void*)
 void GaudiDll::finalize(void*) 
 {
 }
-extern void trigger_load();
+extern void Trigger_load();
 #include "GaudiKernel/FactoryTable.h"
 
 extern "C" FactoryTable::EntryList* getFactoryEntries() {
   static bool first = true;
   if ( first ) {  // Don't call for UNIX
-    trigger_load();
+    Trigger_load();
     first = false;
   }
   return FactoryTable::instance()->getEntries();
 } 
 
-//needed for this
-void FATAL(const char * msg) {
-    std::cerr << "Fatal error from trigger DLL: " << msg << std::endl; exit(-1);
-}
-
-void WARNING(const char * msg) {
-    std::cerr << "Warning message from trigger DLL:" << msg << std::endl;
-}
--- src/mainpage.h
+++ src/mainpage.h
@@ -1,7 +1,7 @@
-//$Header: /nfs/slac/g/glast/ground/cvs/userAlg/src/mainpage.h,v 1.1.1.1 2001/04/01 22:25:06 burnett Exp $
+//$Header: /nfs/slac/g/glast/ground/cvs/Trigger/src/mainpage.h,v 1.1.1.1 2001/05/23 14:56:24 burnett Exp $
 // (Special "header" just for doxygen)
 
-/*! @mainpage  package trigger
+/*! @mainpage  package Trigger
 
 
 
