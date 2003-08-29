57336c42784a5a7f6e0ca203b8e603249755a518
burnett <>
2003-08-29 13:36:34 +0000

    remove obsolete code; lower output level

--- src/Trigger_load.cxx
+++ src/Trigger_load.cxx
@@ -3,13 +3,12 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: $
+*  $Header: /nfs/slac/g/glast/ground/cvs/Trigger/src/Trigger_load.cxx,v 1.4 2002/05/11 22:27:59 burnett Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 DECLARE_FACTORY_ENTRIES(Trigger) {
     DECLARE_ALGORITHM( TriggerAlg );
-    DECLARE_ALGORITHM( ACDthrottle );
 } 
 
