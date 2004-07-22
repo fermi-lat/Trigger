e42d0cb7d87550eddddacd0f7387d0e9034df457
burnett <>
2004-07-22 13:26:38 +0000

    update Gaudi interface

--- src/Trigger_dll.cxx
+++ src/Trigger_dll.cxx
@@ -1,26 +1,8 @@
-//====================================================================
-//
-//====================================================================
+/** @file Trigger_dll.cpp
 
-// DllMain entry point
-#include "GaudiKernel/DllMain.icpp"
-#include <iostream>
-void GaudiDll::initialize(void*) 
-{
-}
+  $Header$
+*/
 
-void GaudiDll::finalize(void*) 
-{
-}
-extern void Trigger_load();
-#include "GaudiKernel/FactoryTable.h"
-
-extern "C" FactoryTable::EntryList* getFactoryEntries() {
-  static bool first = true;
-  if ( first ) {  // Don't call for UNIX
-    Trigger_load();
-    first = false;
-  }
-  return FactoryTable::instance()->getEntries();
-} 
+#include "GaudiKernel/LoadFactoryEntries.h"
+LOAD_FACTORY_ENTRIES(Trigger)
 
