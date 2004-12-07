b6f1491f1d143af552f8ce5a90af2d5dec71f282
heather <>
2004-12-07 23:04:31 +0000

    apply Toby's fix to skip deadtime when reading data in

--- src/TriggerAlg.cxx
+++ src/TriggerAlg.cxx
@@ -2,7 +2,7 @@
 * @file TriggerAlg.cxx
 * @brief Declaration and definition of the algorithm TriggerAlg.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/Trigger/src/TriggerAlg.cxx,v 1.32.2.2 2004/11/18 18:14:03 heather Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/Trigger/src/TriggerAlg.cxx,v 1.32.2.3 2004/12/01 18:41:03 heather Exp $
 */
 
 // Include files
@@ -219,6 +219,9 @@ StatusCode TriggerAlg::caltrigsetup()
 
 bool TriggerAlg::alive(double current_time)
 { 
+    // this is the case when reading back
+    if (m_deadtime <= 0) return true;
+    // ok we actually want to apply a deadtime: in this case *assume* time is increasing!
     return (current_time-m_lastTriggerTime >= m_deadtime);
 }
 
