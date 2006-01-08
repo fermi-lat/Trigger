92215bc8badd82e46fb182a330a9abba39a71cdd
burnett <>
2006-01-08 01:46:41 +0000

    Implement service to manage livetime

--- src/Trigger_load.cxx
+++ src/Trigger_load.cxx
@@ -3,12 +3,13 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/Trigger/src/Trigger_load.cxx,v 1.5 2003/08/29 13:36:34 burnett Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/Trigger/src/Trigger_load.cxx,v 1.6 2004/07/25 21:13:59 burnett Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 DECLARE_FACTORY_ENTRIES(Trigger) {
     DECLARE_ALGORITHM( TriggerAlg );
+    DECLARE_SERVICE( LivetimeSvc );
 } 
 
