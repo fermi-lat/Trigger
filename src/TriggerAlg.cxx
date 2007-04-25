a14b08647b59fcda9f97c31cc3c9afd3501a422c
burnett <>
2007-04-25 02:50:08 +0000

    tag after fixing default prescale factors

--- src/TriggerAlg.cxx
+++ src/TriggerAlg.cxx
@@ -2,7 +2,7 @@
 *  @file TriggerAlg.cxx
 *  @brief Declaration and definition of the algorithm TriggerAlg.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/Trigger/src/TriggerAlg.cxx,v 1.69 2007/04/10 02:53:10 burnett Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/Trigger/src/TriggerAlg.cxx,v 1.70 2007/04/10 21:54:11 burnett Exp $
 */
 
 #include "ThrottleAlg.h"
@@ -157,8 +157,8 @@ TriggerAlg::TriggerAlg(const std::string& name, ISvcLocator* pSvcLocator)
     declareProperty("vetomask",  m_vetomask=1+2+4);  // if thottle it set, veto if trigger masked with these ...
     declareProperty("vetobits",  m_vetobits=1+2);    // equals these bits
 
-    declareProperty("engine",    m_table = "");     // set to "detault"  to use default engine
-    declareProperty("prescale",  m_prescale);        // vector of prescale factors
+    declareProperty("engine",    m_table = "");     // set to "default"  to use default engine table
+    declareProperty("prescale",  m_prescale=std::vector<int>());        // vector of prescale factors
 
     for( int i=0; i<8; ++i) { 
         std::stringstream t; t<< "bit "<< i;
