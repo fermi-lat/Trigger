af371c66d4bdf3d0e138b64eb13a853f92d4ded5
kocian <>
2007-11-27 21:04:25 +0000

    updated documentation

--- src/mainpage.h
+++ src/mainpage.h
@@ -1,29 +1,27 @@
-//$Header: /nfs/slac/g/glast/ground/cvs/Trigger/src/mainpage.h,v 1.11 2007/04/11 01:32:11 burnett Exp $
+//$Header: /nfs/slac/g/glast/ground/cvs/Trigger/src/mainpage.h,v 1.12 2007/09/05 16:43:38 heather Exp $
 // (Special "header" just for doxygen)
 
 /*! @mainpage  package Trigger
 
 Implements 
 - TriggerAlg 
-- ThrottleAlg
 - TrgConfigSvc
+- LivetimeSvc
 
 \section s1 TriggerAlg properties
 TriggerAlg analyzes the digis for trigger conditions, and optionally 
 sets a flag to abort processing of subsequent algorithms in the same sequence. 
 
 @param mask [0xffffffff]  Mask to apply to the trigger word to decide if event is accepted; if 0, accept all; default accept any set bit
-@param deadtime [0.]  Livetime threshold (s). Ignore if zero. 
-
 @param throttle if set, veto when throttle bit is on
 @param vetomask [1+2+4]  if thottle it set, veto if trigger masked with these ...
-@param vetobits[ 1+2]    equals these bits
+@param vetobits [1+2]    equals these bits
 @param engine [""]   specify data source for engine data. "default" and "TrgConfigSvc are other options
 @param prescales []  allow to override the prescales. Should be alist of 12 integers
-
-If the deadtime is enabled, this much time is subtracted from the accumulated livetime associated with the given 
-event, which is copied to the event header. (But note that deadtime is now managed by LivetimeSvc.)
-
+@param applyPrescales [false] if using TrgConfigSvc, do we want to prescale events?
+@param useGltWordForData [false]   Even if a GEM word exists use the Glt word
+@param applyWindowMask [false] Do we want to filter events using the window open mask? Needs to be true for proper GEM simulation
+@param applyDeadtime [false] Filter events based on simulated GEM deadtime supplied by LivetimeSvc
 
 
 
@@ -38,35 +36,34 @@ The trigger word, defined below, is copied to the event header.
         b_LO_CAL=    4,  ///>  single log above low threshold
         b_HI_CAL=    8,  ///> single log above high threshold
         b_ACDH =    16,  ///>  cover or side veto, high threshold ("CNO")
-        b_THROTTLE= 32,  ///>  throttle
-        b_ACDL =     64,  ///>  set if cover or side veto, low threshold
 
-        number_of_trigger_bits = 7, ///> for size of table
+        number_of_trigger_bits = 5, ///> for size of table
 
         GEM_offset = 8  ///> offset to the GEM bits
 @endverbatim
 This new version has the GEM bits coped to the upper 8 bits of the trigger word.
 
-\section s5 LivetimeSvc properties
+\section s4 LivetimeSvc properties
 
-@param Deadtime [25.0e-6]       deadtime to apply to trigger, in sec.
-@param TriggerRate  [2000.]     effective total trigger rate
+@param Deadtime [26.45e-6]       deadtime to apply to trigger for 1-range readout, in sec.
+@param DeadtimeLong [65.4e-6]       deadtime to apply to trigger for 4-range readout, in sec.
+@param clockrate [20e6]    Number of GEM clock ticks in one second.
+@param TriggerRate  [2000.]     effective total trigger rate.
+@param InterleaveMode [true]    Apply efficiency correction for interleave mode.
 
 \section s5 TrgConfigSvc properties
+
 To use the TrgConfigSvc, we first must set up TriggerAlg as follows:
 TriggerAlg.engine="TrgConfigSvc";
 
 Note that there are three mutually exclusive ways to use the TrgConfigSvc:  Using the default configuration, 
 an input XML file, or MOOT
 
-@param useDefaultConfiguration [false]  if set to true use the default config file stored in /src/defaultTrgConfig.xml
+@param configureFrom ["Default"]  Where do we find the configuration? Options are "Default", "File", "Moot".
+@param xmlFile path to the config XML file if configuring from a file.
 
-@param configureFromFile [false] if set to true, we configure using an input XML file
-@param xmlFile path to the config XML file
-
-@param configureFromMoot [false] if set to true, use the MOOT DB to configure
-@param useKeyFromData [false] if set to true use the DB key stored in the event data
-@param fixedKey [1852] if useKeyFromData is false, then use this fixed key for all events
+@param useKeyFromData [true] if set to true use the DB key stored in the event data to find configuration.
+@param fixedKey [1852] if useKeyFromData is false, then use this fixed key for all events.
 
   <hr>
   \section requirements requirements
