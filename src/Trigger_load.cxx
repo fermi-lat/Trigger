31b9c60f0d87ed6499ceebe113e76512a23b91db
usher <>
2008-12-18 23:42:13 +0000

    Rearrangement of Trigger... TriggerInfoAlg now calculates the trigger primitives in the same manner previously done in TriggerAlg, TriggerAlg then uses this information (from TDS object TriggerInfo). This always insertion of a step in between for the overlay

--- src/Trigger_load.cxx
+++ src/Trigger_load.cxx
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/Trigger/src/Trigger_load.cxx,v 1.9 2007/09/05 16:43:38 heather Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/Trigger/src/Trigger_load.cxx,v 1.10 2008/06/13 22:19:14 echarles Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -12,5 +12,6 @@ DECLARE_FACTORY_ENTRIES(Trigger) {
     DECLARE_ALGORITHM( TriggerAlg );
     DECLARE_ALGORITHM( TriRowBitsAlg );
     DECLARE_SERVICE( LivetimeSvc );
+    DECLARE_ALGORITHM( TriggerInfoAlg );
 } 
 
