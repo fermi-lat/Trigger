c6b0b9f6d1beb3db3cc00439207896ca6e09c058
jrb <>
2012-08-17 18:40:36 +0000

    patches for Windows no-install-headers

--- TriggerLib.py
+++ TriggerLib.py
@@ -1,7 +1,9 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/Trigger/TriggerLib.py,v 1.2 2008/10/27 18:42:03 ecephas Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/Trigger/TriggerLib.py,v 1.3 2009/11/10 23:41:06 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['Trigger'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'Trigger') 
     env.Tool('addLibrary', library = env['gaudiLibs'])
     env.Tool('addLibrary', library = env['clhepLibs'])
     env.Tool('EventLib')
@@ -11,5 +13,7 @@ def generate(env, **kw):
     env.Tool('CalXtalResponseLib')
     env.Tool('LdfEventLib')
     env.Tool('identsLib')
+    if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+        env.Tool('findPkgPath', package = 'enums')
 def exists(env):
     return 1;
